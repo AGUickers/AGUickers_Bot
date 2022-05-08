3a315696250321924d61dac4c3b5777160549095
Inspector Soviet <www.alex.avil@mail.ru>
2022-05-08 16:39:02 +0400

    debug

--- app.js
+++ app.js
@@ -311,6 +311,7 @@ bot.onText(/\/toggle/, (msg, match) => {
         console.log(option)
         //Search for the option in the database
         var value = settings.prepare("SELECT value FROM settings WHERE option = ?").get(option);
+        console.log(value.value);
         if (value.value == "true") {
             settings.prepare("UPDATE settings SET value = 'false' WHERE option = ?").run(option);
             bot.answerCallbackQuery(callbackQuery.id, messages.messages.toggled_off);
